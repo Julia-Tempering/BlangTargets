8c279e01b7d83e4d6bf4f108e7a05f24e4a903c2
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-08-21 10:30:59 -0700

    Set of targets from JRSSB paper

---
+++ src/BlangTargets.jl
@@ -0,0 +1,34 @@
+module BlangTargets
+
+using Pigeons 
+using CSV
+
+for blang_lib in Pigeons.precompiled_blang_libs 
+    Pigeons.setup_blang(blang_lib) 
+end
+
+function build_targets_db() 
+    result = Dict{Symbol, Any}()
+    csv_file = "$(Pigeons.mpi_settings_folder())/blangDemos/src/main/resources/demos/models.csv"
+    for row in CSV.File(csv_file)
+        result[Symbol(row.name)] = row
+    end
+    result[:sitka] = nothing
+    return result
+end
+const target_db = build_targets_db() 
+
+provide_targetIds() = keys(target_db)
+function provide_target(targetId::Symbol)
+    if targetId == :sitka
+        return Pigeons.blang_sitka()
+    end
+    specs = target_db[targetId]
+    model_options = ismissing(specs.arguments) ? `` : Cmd(Base.shell_split(specs.arguments))
+    return Pigeons.BlangTarget(`$(Pigeons.blang_executable("blangDemos", specs.class)) $model_options`)
+end
+
+Pigeons.extract_sample(state::Pigeons.StreamState, log_potential) = [log_potential(state)]
+Pigeons.sample_names(::Pigeons.StreamState, _) = [:logd]
+
+end
